b44f1012495dba7d11db0fe15ef8e7fb95f1d606
antonv <toxa07@gmail.com>
2017-10-26 09:05:10 +0200

    initial

---
+++ conemu.js
@@ -0,0 +1,67 @@
+class ConemuCommandBuilder {
+  constructor(path) {
+    this.conemuPath = `"${path}"`;
+    this.runMode = "-run";
+    this.newConsole = false;
+    this.tabName = null;
+    this.workDir = null;
+    this.shellName = "{Shells::cmd}";
+    this.command = `echo hello world`;
+  }
+
+  runList() {
+    this.runMode = "-runlist";
+    return this;
+  }
+
+  inNewConsole() {
+    this.newConsole = true;
+    return this;
+  }
+
+  inDirectory(workDir) {
+    this.workDir = workDir;
+    return this;
+  }
+
+  withTabName(tabName) {
+    this.tabName = tabName;
+    return this;
+  }
+
+  withShell(shellName) {
+    this.shellName = `{${shellName}}`;
+    return this;
+  }
+
+  withCommand(command) {
+    this.command = command;
+    return this;
+  }
+
+  build() {
+    let execute = `${this.conemuPath} `;
+    execute += this.runMode;
+    if (this.inNewConsole) {
+      execute += " new_console";
+    }
+    if (this.tabName) {
+      execute += `:t:"${this.tabName}"`;
+    }
+    if (this.workDir) {
+      execute += `:d:${this.workDir}`;
+    }
+    if (this.shellName) {
+      execute += ` ${this.shellName}`;
+    }
+    if (this.command) {
+      execute += ` ${this.command}`;
+    }
+
+    return execute;
+  }
+}
+
+module.exports = {
+  ConemuCommandBuilder
+};
---
+++ index.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const childProcess = require("child_process");
+const util = require("util");
+const SHELLS = require("./shells");
+const { ConemuCommandBuilder } = require("./conemu");
+const execFile = util.promisify(require("child_process").execFile);
+
+console.log("Hello from node");
+
+const ConEmuPath = '"c:/tools/ConEmu/ConEmu64.exe"';
+const shell = SHELLS.cmd;
+const path = "c:/tools";
+
+const builder = new ConemuCommandBuilder("c:/tools/ConEmu/ConEmu64.exe");
+
+const exit = shouldExit => (shouldExit ? shell.paramExit : shell.paramNoExit);
+// const tabName = name => `:t:${name}`;
+// const workDir = dir => `:d:${dir}`;
+
+// let execute = `-runlist ${shell.executable} ${exit(false)} npm -g outdated`;
+// execute = `-run -new_console${tabName("test")} {Shells::bash}`;
+
+const command = builder
+  .inNewConsole()
+  .withTabName("npm test")
+  .inDirectory("c:/tools")
+  .withCommand("npm -g outdated")
+  .build();
+
+console.log("command to execute \n", command);
+childProcess.exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(stdout);
+  console.log(stderr);
+});
+
+const getVersion = () =>
+  execFile("node", ["--version"])
+    .then(({ stdout }) => {
+      console.log(stdout);
+      process.exit(0);
+    })
+    .catch(({ stdout, stderr }) => {
+      console.log(stdout);
+      console.error(stderr);
+      process.exit(1);
+    });
+
+getVersion();
