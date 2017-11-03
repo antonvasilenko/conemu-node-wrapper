67825f7b3c8ff8319a4bc9c760304b7ed98acf6e
antonv <toxa07@gmail.com>
2017-11-03 09:04:28 +0100

    fix test run.

--- src/conemu.spec.js
+++ src/conemu.spec.js
@@ -2,9 +2,21 @@ const { expect } = require("chai");
 const { ConemuCommandBuilder } = require("./conemu");
 
 describe("ConemuCommandBuilder", () => {
-  describe("ctor", () => {
-    it("should fail without path", () => {
-      expect(() => new ConemuCommandBuilder()).to.throw(Error);
+  it("should fail without path without path to conemu", () => {
+    expect(() => new ConemuCommandBuilder()).to.throw(Error);
+  });
+
+  describe("with correct path", () => {
+    it("should build correct command with all specified params", () => {
+      const command = new ConemuCommandBuilder("c:/tools/conemu.exe")
+        .inNewConsole()
+        .withTabName("tab name")
+        .inDirectory("c:/tools")
+        .withCommand("npm -g outdated")
+        .build();
+      expect(command).to.be.equal(
+        '"c:/tools/conemu.exe" -run -new_console:t:"tab name":d:c:/tools npm -g outdated'
+      );
     });
   });
 });
--- src/index.js
+++ src/index.js
@@ -6,46 +6,52 @@ const SHELLS = require("./shells");
 const { ConemuCommandBuilder } = require("./conemu");
 const execFile = util.promisify(require("child_process").execFile);
 
-console.log("Hello from node");
+const isInTest = typeof global.it === "function";
 
-const ConEmuPath = '"c:/tools/ConEmu/ConEmu64.exe"';
-const shell = SHELLS.cmd;
-const path = "c:/tools";
+const sandbox = () => {
+  console.log("Hello from node");
 
-const builder = new ConemuCommandBuilder("c:/tools/ConEmu/ConEmu64.exe");
+  const ConEmuPath = '"c:/tools/ConEmu/ConEmu64.exe"';
+  const shell = SHELLS.cmd;
+  const path = "c:/tools";
 
-const exit = shouldExit => (shouldExit ? shell.paramExit : shell.paramNoExit);
+  const builder = new ConemuCommandBuilder("c:/tools/ConEmu/ConEmu64.exe");
 
-// let execute = `-runlist ${shell.executable} ${exit(false)} npm -g outdated`;
-// execute = `-run -new_console${tabName("test")} {Shells::bash}`;
+  const exit = shouldExit => (shouldExit ? shell.paramExit : shell.paramNoExit);
 
-const command = builder
-  .inNewConsole()
-  .withTabName("npm test")
-  .inDirectory("c:/tools")
-  .withCommand("npm -g outdated")
-  .build();
+  // let execute = `-runlist ${shell.executable} ${exit(false)} npm -g outdated`;
+  // execute = `-run -new_console${tabName("test")} {Shells::bash}`;
 
-console.log("command to execute \n", command);
-childProcess.exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(stdout);
-  console.log(stderr);
-});
+  const command = builder
+    .inNewConsole()
+    .withTabName("test-service:serve")
+    .inDirectory("c:/tools")
+    .withCommand("npm -g outdated")
+    .build();
 
-// const getVersion = () =>
-//   execFile("node", ["--version"])
-//     .then(({ stdout }) => {
-//       console.log(stdout);
-//       process.exit(0);
-//     })
-//     .catch(({ stdout, stderr }) => {
-//       console.log(stdout);
-//       console.error(stderr);
-//       process.exit(1);
-//     });
+  console.log("command to execute \n", command);
+  childProcess.exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(stdout);
+    console.log(stderr);
+  });
 
-// getVersion();
+  // const getVersion = () =>
+  //   execFile("node", ["--version"])
+  //     .then(({ stdout }) => {
+  //       console.log(stdout);
+  //       process.exit(0);
+  //     })
+  //     .catch(({ stdout, stderr }) => {
+  //       console.log(stdout);
+  //       console.error(stderr);
+  //       process.exit(1);
+  //     });
+
+  // getVersion();
+};
+
+!isInTest && sandbox();
