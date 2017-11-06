f754f6828cf4262dfa95d157b172e5f4eaee35ce
antonv <toxa07@gmail.com>
2017-11-06 20:40:17 +0100

    some other commands to try.

--- conemu_sandbox.bat
+++ conemu_sandbox.bat
@@ -1,1 +1,7 @@
 c:\tools\ConEmu\ConEmu\ConEmuC.exe /AUTOATTACH && cmd /k -cur_console node -v
+
+start "MyWindow" cmd /c "ping localhost & ipconfig & pause"
+
+start "MyWindow" cmd /k "ping localhost & ipconfig & pause"
+
+cmd /k "c:\tools\ConEmu\ConEmu\ConEmuC.exe /AUTOATTACH && node -v"
